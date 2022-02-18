78f0e07c3e5f6a93e3c633b4c53cfe8c6c396c10
DarkedgeTH <darkedgeth@gmail.com>
2022-02-17 21:00:49 -0500

    set up return in App.js and added Info component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, {useState, useEfect, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
+import Character from './components/Character'
+import Info from './components/Info'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters, setCharacters] = useState([]);
-  const [currentCharacterId, setCurrentCharacterId] = useState(null);
+  const [characters, setCharacters] = useState([])
+  const [currentCharacterId, setCurrentCharacterId] = useState(null)
 
   const openInfo = id => {
     setCurrentCharacterId(id);
@@ -15,7 +17,7 @@ const App = () => {
   const closeInfo = () => {
     setCurrentCharacterId(null);
   }
-  
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -31,9 +33,12 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      {characters.length > 0 ? characters.map(char => {
-        return <Character />
-      })}
+      {characters.length > 0 ? characters.map(character => {
+        return <Character info={character} key={character.id} openInfo={openInfo} />
+      }) : <h1>LOADING</h1>}
+      {
+        currentCharacterId && <Info characterId={currentCharacterId} closeInfo={closeInfo} />
+      }
     </div>
   );
 }
