4500d2a49aa6712736a7690e039ac3b788ac6cd6
DarkedgeTH <darkedgeth@gmail.com>
2022-02-17 21:15:30 -0500

    initiated useEffect in Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,4 +4,12 @@ import axios from 'axios';
 export default function Info(props) {
     const { characterId, closeInfo } = props;
     const [info, setInfo] = useState(null);
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/people/${props.characterId}`)
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => console.error(err))
+    }, [characterId])
 }
