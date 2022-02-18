fa81c95e345b207681e0c904b6a6b34ac8525e53
DarkedgeTH <darkedgeth@gmail.com>
2022-02-17 20:49:30 -0500

    added additional state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [characters, setCharacters] = useState([]);
+  const [currentCharacterId, setCurrentCharacterId] = useState(null);
+
+  const openInfo = id => {
+    setCurrentCharacterId(id);
+  }
+
+  const closeInfo = () => {
+    setCurrentCharacterId(null);
+  }
+  
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -13,7 +23,7 @@ const App = () => {
   useEffect(() => {
     axios.get('https://swapi.dev/api/people')
     .then(res => {
-      console.log(res.data);
+      setCharacters(res.data);
     })
     .catch(err => console.error(err))
   }, [])
@@ -21,6 +31,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.length > 0 ? characters.map(char => {
+        return <Character />
+      })}
     </div>
   );
 }
