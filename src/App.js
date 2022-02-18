02e66c27bbbbfa372826be24c4bb05d5e5cf048d
DarkedgeTH <darkedgeth@gmail.com>
2022-02-17 20:33:03 -0500

    added and implemented useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState, useEfect, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characters, setCharacters] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => console.error(err))
+  }, [])
 
   return (
     <div className="App">
