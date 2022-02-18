52a7e6b52bfb66b617f92c60732f34b12ee42af7
DarkedgeTH <darkedgeth@gmail.com>
2022-02-17 20:45:13 -0500

    set up Character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,16 @@
+import React from 'react';
+
 // Write your Character component here
+
+const Character = props => {
+    return (
+        <div classname='character'>
+            {props.info.name}
+            <button onClick={() => props.openInfo(props.info.id)}>
+                See Info
+            </button>
+        </div>
+    )
+}
+
+export default Character;
